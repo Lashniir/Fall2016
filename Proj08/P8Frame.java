c60c2aa18d9e6eca197cd21d14e32d191c6e897f
AFoiles <foiles.alex@gmail.com>
2016-12-14 11:43:31 -0600

    Posting Proj08. Not Finished

---
+++ Proj08/P8Frame.java
@@ -0,0 +1,189 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.sql.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+
+public class P8Frame extends JFrame {
+
+    private static JButton prevBT = new JButton("<--");
+    private static JButton nextBT = new JButton("-->");
+    private JButton exitBT = new JButton("Exit");
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu menu = new JMenu("Database");
+    private JMenuItem openMI = new JMenuItem("Open");
+    private JMenuItem closMI = new JMenuItem("Close");
+    private String[] record = new String[8];
+    
+    private JButton updaBT = new JButton("Update");
+    private JButton cancBT = new JButton("Cancel");
+    private JButton deleBT = new JButton("Delete");
+    private JButton newBT = new JButton("New");
+
+    public P8Frame() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        P8Panel cPanel = new P8Panel();
+        setLayout(new BorderLayout());
+        add(cPanel, BorderLayout.CENTER);
+
+        JPanel bPanel = new JPanel();
+        bPanel.add(prevBT);
+        prevBT.setEnabled(false);
+        bPanel.add(nextBT);
+        nextBT.setEnabled(false);
+        bPanel.add(exitBT);
+        bPanel.add(updaBT);
+        bPanel.add(cancBT);
+        bPanel.add(deleBT);
+        deleBT.setEnabled(false);
+        bPanel.add(newBT);
+        newBT.setEnabled(false);
+        add(bPanel, BorderLayout.SOUTH);
+
+        menuBar.add(menu);
+        menu.add(openMI);
+        menu.add(closMI);
+        closMI.setEnabled(false);
+        menu.setMnemonic(KeyEvent.VK_D);
+        openMI.setMnemonic(KeyEvent.VK_O);
+        closMI.setMnemonic(KeyEvent.VK_C);
+
+        add(menuBar, BorderLayout.NORTH);
+
+        exitBT.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        openMI.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                DBDialog dbDial = new DBDialog(P8Frame.this);
+                dbDial.setVisible(true);
+                if (dbDial.getOK() == true) {
+                    try {
+                        Class.forName("org.gjt.mm.mysql.Driver");
+                        String dbconn = "jdbc:mysql://" + dbDial.getHostName() + ":3300/"
+                                + dbDial.getDBName();
+                        Connection conn
+                                = DriverManager.getConnection(dbconn, dbDial.getUsername(),
+                                        dbDial.getPassword());
+                        Statement stmt = conn.createStatement();
+                        String query1 = "select * from " + dbDial.getTableName();
+                        ResultSet rset = stmt.executeQuery(query1);
+                        openMI.setEnabled(false);
+                        closMI.setEnabled(true);
+                        nextBT.setEnabled(true);
+                        rset.next();
+                        record[0] = rset.getString(3);
+                        record[1] = rset.getString(2);
+                        record[2] = rset.getString(4);
+                        record[3] = rset.getString(5);
+                        record[4] = rset.getString(6);
+                        record[5] = rset.getString(7);
+                        record[6] = rset.getString(8);
+                        record[7] = rset.getString(9);
+                        cPanel.setUIData(record);
+                        prevBT.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                try {
+                                    if (rset.isFirst()) {
+                                        prevBT.setEnabled(false);
+                                    } else {
+                                        prevBT.setEnabled(true);
+                                        nextBT.setEnabled(true);
+                                        rset.previous();
+                                        for (int i = 0; i < record.length; i++) {
+
+                                        }
+                                        record[0] = rset.getString(3);
+                                        record[1] = rset.getString(2);
+                                        record[2] = rset.getString(4);
+                                        record[3] = rset.getString(5);
+                                        record[4] = rset.getString(6);
+                                        record[5] = rset.getString(7);
+                                        record[6] = rset.getString(8);
+                                        record[7] = rset.getString(9);
+                                        cPanel.aidCheck.setSelected(false);
+                                        cPanel.fullTCheck.setSelected(false);
+                                        cPanel.OOSCheck.setSelected(false);
+                                        cPanel.setUIData(record);
+                                        if (rset.isFirst()) {
+                                            prevBT.setEnabled(false);
+                                        }
+                                    }
+                                } catch (SQLException sqle) {
+                                    System.out.println(sqle);
+                                }
+                            }
+                        });
+                        nextBT.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                try {
+                                    if (rset.isLast()) {
+                                        nextBT.setEnabled(false);
+                                    } else {
+                                        nextBT.setEnabled(true);
+                                        prevBT.setEnabled(true);
+                                        rset.next();
+                                        record[0] = rset.getString(3);
+                                        record[1] = rset.getString(2);
+                                        record[2] = rset.getString(4);
+                                        record[3] = rset.getString(5);
+                                        record[4] = rset.getString(6);
+                                        record[5] = rset.getString(7);
+                                        record[6] = rset.getString(8);
+                                        record[7] = rset.getString(9);
+                                        cPanel.dummyRB.setEnabled(true);
+                                        cPanel.aidCheck.setSelected(false);
+                                        cPanel.fullTCheck.setSelected(false);
+                                        cPanel.OOSCheck.setSelected(false);
+                                        cPanel.setUIData(record);
+                                        if (rset.isLast()) {
+                                            nextBT.setEnabled(false);
+                                        }
+                                    }
+                                } catch (SQLException sqle) {
+                                    System.out.println(sqle);
+                                }
+                            }
+                        });
+                        closMI.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                P8Panel.p8ClearUI();
+                                try {
+                                    while (rset.isFirst() != true) {
+                                        rset.previous();
+                                    }
+                                } catch (SQLException sqle) {
+                                    System.out.println(sqle);
+                                }
+                            }
+                        });
+                    } catch (ClassNotFoundException cnfe) {
+                        System.out.println(cnfe);
+                    } catch (SQLException sqle) {
+                        System.out.println(sqle);
+                    }
+                }
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        P8Frame frame = new P8Frame();
+        frame.setSize(500, 280);
+        frame.setVisible(true);
+    }
+}
---
+++ Proj08/P8Panel.java
@@ -0,0 +1,168 @@
+import java.awt.GridLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+
+public class P8Panel extends JPanel {
+
+    public static final int UI_VALID = 0;
+    public static final int UI_INVALID_NO_FIRST_NAME = 1;
+    public static final int UI_INVALID_NO_LAST_NAME = 2;
+    public static final int UI_INVALID_NO_MAJOR = 3;
+    public static final int UI_INVALID_NO_DEGREE = 4;
+    public static final int UI_INVALID_NO_YEAR = 5;
+    public static final int UI_VALID_BUT_NEEDS_HELP = 6;
+
+    private static String[] depts = {"Select One", "Art", "Biology", "Buisness",
+        "Chemistry", "CIS", "Economics", "History", "Kinesiology",
+        "Mathematics", "Political Science"};
+    private static String[] years = {"Select One", "1999", "2000", "2001", "2002",
+        "2003", "2004", "2005", "2006,2007", "2008", "2009", "2010", "2011", "2012",
+        "2013", "2014", "2015", "2016", "2017"};
+    private JLabel firstNameLB = new JLabel("First Name: ");
+    private JLabel lastNameLB = new JLabel("Last Name: ");
+    private JLabel deptLB = new JLabel("Major Dept: ");
+    private static JLabel degreeLB = new JLabel("Degree: ");
+    private JLabel yearLB = new JLabel("Year: ");
+    private static JTextField firstNameTF = new JTextField();
+    private static JTextField lastNameTF = new JTextField();
+    private static JComboBox deptCB = new JComboBox(depts);
+    private static JComboBox yearCB = new JComboBox(years);
+    private static JRadioButton aARB = new JRadioButton("AA");
+    private JRadioButton bARB = new JRadioButton("BA");
+    private JRadioButton bSRB = new JRadioButton("BS");
+    static JRadioButton dummyRB = new JRadioButton();
+    private ButtonGroup bg = new ButtonGroup();
+    static JCheckBox aidCheck = new JCheckBox("On Financial Aid");
+    static JCheckBox fullTCheck = new JCheckBox("Full Time");
+    static JCheckBox OOSCheck = new JCheckBox("Out of State");
+    private JPanel panel1 = new JPanel();
+    private JPanel panel2 = new JPanel();
+    private JPanel panel3 = new JPanel();
+    private JPanel panel4 = new JPanel();
+    private JPanel panel5 = new JPanel();
+
+    public P8Panel() {
+        this.setLayout(new GridLayout(5, 1));
+        this.add(panel1);
+        this.add(panel2);
+        this.add(panel3);
+        this.add(panel4);
+        this.add(panel5);
+
+        bg.add(aARB);
+        bg.add(bARB);
+        bg.add(bSRB);
+        bg.add(dummyRB);
+
+        dummyRB.setSelected(true);
+
+        deptCB.setSelectedIndex(0);
+        yearCB.setSelectedIndex(0);
+
+        firstNameTF.setColumns(13);
+        lastNameTF.setColumns(13);
+
+        panel1.add(firstNameLB);
+        panel1.add(firstNameTF);
+        panel2.add(lastNameLB);
+        panel2.add(lastNameTF);
+        panel3.add(deptLB);
+        panel3.add(deptCB);
+        panel4.add(degreeLB);
+        panel4.add(aARB);
+        panel4.add(bARB);
+        panel4.add(bSRB);
+        panel4.add(yearLB);
+        panel4.add(yearCB);
+        panel5.add(aidCheck);
+        panel5.add(fullTCheck);
+        panel5.add(OOSCheck);
+    }
+
+    public static int p8ValidateUI() {
+        if (firstNameTF.getText().trim().equals("")) {
+            return 1;
+        } else if (lastNameTF.getText().trim().equals("")) {
+            return 2;
+        } else if (deptCB.getSelectedIndex() == 0) {
+            return 3;
+        } else if (dummyRB.isSelected()) {
+            return 4;
+        } else if (yearCB.getSelectedIndex() == 0) {
+            return 5;
+        } else if (deptCB.getSelectedIndex() == 12) {
+            return 6;
+        } else {
+            return 0;
+        }
+    }
+
+    public static void p8ClearUI() {
+        firstNameTF.setText(null);
+        lastNameTF.setText(null);
+        deptCB.setSelectedIndex(0);
+        yearCB.setSelectedIndex(0);
+        dummyRB.setSelected(true);
+        aidCheck.setSelected(false);
+        fullTCheck.setSelected(false);
+        OOSCheck.setSelected(false);
+    }
+
+    public static void focusFirst() {
+        firstNameTF.requestFocus();
+    }
+
+    public static void focusLast() {
+        lastNameTF.requestFocus();
+    }
+
+    public static void focusDept() {
+        deptCB.requestFocus();
+    }
+
+    public static void focusDegree() {
+        aARB.requestFocus();
+    }
+
+    public static void focusYear() {
+        yearCB.requestFocus();
+    }
+
+    public void setUIData(String[] strArray) {
+        firstNameTF.setText(strArray[0]);
+        lastNameTF.setText(strArray[1]);
+        for (int i = 0; i < depts.length; i++) {
+            if (depts[i].equals(strArray[2])) {
+                deptCB.setSelectedItem(depts[i]);
+            }
+        }
+        if (strArray[3].equals("AA")) {
+            aARB.setSelected(true);
+        } else if (strArray[3].equals("BA")) {
+            bARB.setSelected(true);
+        } else if (strArray[3].equals("BS")) {
+            bSRB.setSelected(true);
+        } else {
+            dummyRB.setSelected(true);
+        }
+        for (int i = 0; i < years.length; i++) {
+            if (years[i].equals(strArray[4])) {
+                yearCB.setSelectedItem(years[i]);
+            }
+        }
+        if (strArray[5].equals("T")) {
+            aidCheck.setSelected(true);
+        }
+        if (strArray[6].equals("T")) {
+            fullTCheck.setSelected(true);
+        }
+        if (strArray[7].equals("T")) {
+            OOSCheck.setSelected(true);
+        }
+    }
+}
